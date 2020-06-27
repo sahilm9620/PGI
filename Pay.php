c07a6e92349e7120de51acbd83ce0b9e803191d3
sahilm9620 <65106833+sahilm9620@users.noreply.github.com>
2020-06-27 09:14:21 +0530

    update the Pay.php
    change to test account

--- Pay.php
+++ Pay.php
@@ -1,29 +1,35 @@
-<?php 
-$purpose = "Payment";
-$amount = $_POST["amount"];
-$name = $_POST["name"];
-//$phone = $_POST["phone"];
-$email = $_POST["email"];
-include 'src/instamojo.php';
-$api = new Instamojo\Instamojo('test_6620ec1c9ff2cd530b90d1ec36d', 'test_69417cae46934ed9d74d90f1d43','https://www.instamojo.com/api/1.1/');
+<?php
+
+$Name=$_POST['Name'];
+$Email=$_POST['Email'];
+$Amount=$_POST['Amount'];
+$Phone=$_POST['phone'];
+$purpose='Donation';
+
+
+include 'instamojo/Instamojo.php';
+$api = new Instamojo\Instamojo('test_3743a7a31d4c8d710f488ba8e39', 'test_2df6effdc4b9b2e6dd53dddd5a1', 'https://test.instamojo.com/api/1.1/');
+
 try {
     $response = $api->paymentRequestCreate(array(
         "purpose" => $purpose,
-        "amount" => $amount,
-        "buyer_name" => $name,
-        "phone" => $phone,
-		"email" => $email,
+        "amount" => $Amount,
         "send_email" => true,
+        "email" => $Email,
+        "buyer_name" =>$Name,
+        "phone"=>$Phone,
         "send_sms" => true,
-        'allow_repeated_payments' => false,
-        "redirect_url" => "https://studentstutorial.com/instamojo/thankyou.php",
-        "webhook" => "https://studentstutorial.com/instamojo/webhook.php"
+        "allow_repeated_payments" =>false,
+        "redirect_url" => "https://payment-gateway-integration.000webhostapp.com/redirect.php"
         ));
-   $pay_ulr = $response['longurl'];
-    header("Location: $pay_ulr");
-    exit();
-}
-catch (Exception $e) {
-    print('Error: ' . $e->getMessage());
-}     
- ?>
+    //print_r($response);
+    $pay_url=$response['longurl'];
+    header("location: $pay_url");
+	}
+	catch (Exception $e) {
+	    print('Error: ' . $e->getMessage());
+	}
+
+
+
+?>
